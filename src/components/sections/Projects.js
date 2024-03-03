9f4b0ca716087694a19bd82fe3d38dce0ab12705
LuizHenriqueSantin <124394078+LuizHenriqueSantin@users.noreply.github.com>
2024-03-03 00:34:54 -0300

    Update Projects.js

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -12,7 +12,7 @@ function Projects(){
             <Card lnk="https://desafio3-iota.vercel.app" icon={card1} text="Projeto de página para organizaçao de tarefas diárias, sendo possivel adiconar, editar e excluir tarefas, e contendo contador de progresso em tarefas concluídas"/>
             <Card lnk="https://neurosync-ecru.vercel.app" icon={card2} text="Projeto em desenvolvimento, para empresa de psicologia com sessões para contato, apresentação da empresa e dos funcionários e explicação sobre os procedimentos realizados, desenvolvido utilizando o ReactJS"/>
             <Card lnk="https://thebatman-six.vercel.app" icon={card3} text="Projeto de site simulando a venda de ingressos para o filme The Batman, com trailer, imagens, simulador de comentários e informações sobre os atores principais. Desenvolvido por meio do ReactJS"/>
-            <Btn2 lk="https://github.com/LuizHenriqueSantin" text="Ver repositório completo completo!"/>
+            <Btn2 lk="https://github.com/LuizHenriqueSantin" text="Ver repositório completo!"/>
         </div>
     )
 }
